e2ce8790adaaa177c201da64e4668091c835121d
Adebayo Oyeleye <adebayo.oyeleye@gmail.com>
2025-12-18 21:27:51 -0500

    Day 1 commit - Foundation Setup

---
+++ backend/content-service/src/main/java/com/storyplatform/contentservice/HealthController.java
@@ -0,0 +1,13 @@
+package com.storyplatform.contentservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/api/health")
+    public String healthCheck() {
+        return "Story Platform Backend is UP!";
+    }
+}
